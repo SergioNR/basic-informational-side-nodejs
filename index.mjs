147dd19df38486c3d2fef2d27ad137edfaed7418
Sergio Navarro <s.navarroredondo@gmail.com>
2024-07-12 23:50:39 +0200

    UPDATE axios request to include more detailed syntax

--- index.mjs
+++ index.mjs
@@ -1,5 +1,14 @@
 import axios from "axios";
 
-axios.get("http://localhost:3000").then((res) => {
+
+axios.request({
+    mode: "no-cors",
+    method: "POST",
+    url: "http://localhost:3000/about.html?name=John&age=25",
+    data: {
+        name: "John Doe",
+        age: 25,
+    },
+}).then((res) => {
     console.log(res.data);
 });
