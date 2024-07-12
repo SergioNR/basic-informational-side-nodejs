c8bff6d9787ce32893cd6723c3e0b8a26d92a3ca
Sergio Navarro <s.navarroredondo@gmail.com>
2024-07-12 23:43:49 +0200

    ADD server.mjs

---
+++ server.mjs
@@ -0,0 +1,14 @@
+import * as http from 'node:http';
+
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+
+    res.end('Hello World\n')
+})
+
+server.listen(process.env.PORT || 3000, () => {
+    console.log('Server is running')
+});
