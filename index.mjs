5154815a707959436984f07945f6dd2d365948fd
Sergio Navarro <s.navarroredondo@gmail.com>
2024-07-13 22:37:45 +0200

    ADD DEBUG axios calls for index.html, contact-me.html and about.html

--- index.mjs
+++ index.mjs
@@ -2,16 +2,46 @@ import axios from "axios";
 
 
 
-
+// ABOUT.HTML REQUEST
 axios.request({
     mode: "no-cors",
     method: "POST",
     url: "/about.html",
     baseURL: "http://localhost:3000/",
     data: {
-        name: "John Doe",
-        age: 25,
     },
 }).then((res) => {
-    window.document.body.innerHTML = res.data; //* This will render the response in the browser
+    console.log(res.data); //* This will log the response to the console
+
 });
+
+// CONTACT-ME.HTML REQUEST
+
+axios.request({
+    mode: "no-cors",
+    method: "POST",
+    url: "/contact-me.html",
+    baseURL: "http://localhost:3000/",
+    data: {
+    },
+}).then((res) => {
+    console.log(res.data); //* This will log the response to the console
+
+});
+
+// INDEX.HTML REQUEST
+
+axios.request({
+    mode: "no-cors",
+    method: "POST",
+    url: "/index.html",
+    baseURL: "http://localhost:3000/",
+    data: {
+    },
+}).then((res) => {
+    console.log(res.data); //* This will log the response to the console
+
+});
+
+
+
